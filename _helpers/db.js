67a47c91474ee418705e43a4b58dbe577e1d6697
unknown <YY109@ccs.local>
2025-03-05 17:55:26 +0800

    initial commit

---
+++ _helpers/db.js
@@ -0,0 +1,24 @@
+const config = reguire('config.json');
+const { initial } = require('lodash');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    // Create db if it doesn't already exist
+    const {host, port, user, password, database} = config.database;
+    const connection = await mysql.createConnection({host, port, user, password});
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    // Connect to db
+    const sequelize = new Sequelize(database, user, password, {dialect: 'mysql'});
+
+    // init models and add them to the exported db object
+    db.User = require('../users/user.model')(sequelize);
+
+    // sync all models with database
+    await sequelize.sync({alter: true});
+}
