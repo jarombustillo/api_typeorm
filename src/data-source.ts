3f68b3eed83a9bee2a3486446ba6da7671d3d9d8
jarombustillo <jarombustillo222@gmail.com>
2025-03-11 22:45:42 +0800

    Converted the project to Typescript and TypeORM

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./users/user.entity"; 
+
+export const AppDataSource = new DataSource({
+    type: "mysql", 
+    host: process.env.HOST || "localhost",
+    port: 3306,
+    username: process.env.USER || "root",
+    password: process.env.PASSWORD || "Jarom12345@",
+    database: process.env.DATABASE || "node-mysql-crud-api",
+    synchronize: true, // Set to false in production!
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+});
